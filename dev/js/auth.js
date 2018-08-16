ce6d46bfa351e519f6f7c7af976e0bf1bee937cc
Alexander Samoylov <smlwmy@gmail.com>
2018-08-16 11:13:37 +0300

    registration JS done

--- dev/js/auth.js
+++ dev/js/auth.js
@@ -15,5 +15,29 @@ $(function() {
         $('.register').hide();
       }
     });
+
+    /* register button */
+    $('.register-button').on('click', function(e){
+      e.preventDefault();
+
+      var data = {
+        login: $('#register-login').val(),
+        password: $('#register-password').val(),
+        passwordConfirm: $('#register-password-confirm').val()
+      }
+
+      $.ajax({
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        url: '/api/auth/register'
+      }).done(function(data){
+        console.log(data);
+      })
+    });
   });
-  /* eslint-enable no-undef */
+
+
+
+
+/* eslint-enable no-undef */
