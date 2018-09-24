0203d74a632c12c317dd8f3b5932a05eb053e1d0
Alexander Samoylov <smlwmy@gmail.com>
2018-09-24 18:54:52 +0500

    create pagination

--- app.js
+++ app.js
@@ -55,20 +55,12 @@ app.use(
 );
 
 // routers
-app.get('/', function (req, res) {
-  const id = req.session.userId;
-  const login = req.session.userLogin;
-  res.render('index', {
-    user: {
-      id,
-      login
-    }
-  }); 
-});
+
 
 // Routers
 app.use('/api/auth', routes.auth);
 app.use('/post', routes.post);
+app.use('/', routes.archive)
 
 //catch 404 and forward to error handler
 app.use((req, res, next) => {
