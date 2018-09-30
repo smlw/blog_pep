670fd69e867fc9d7cd100a9315ffcaad83274abc
Alexander Samoylov <smlwmy@gmail.com>
2018-09-30 15:47:36 +0500

    refactor then() to async/await

--- routes/archive.js
+++ routes/archive.js
@@ -4,7 +4,7 @@ const models = require('../models')
 
 const config = require('../config');
 
-function posts(req, res) {
+async function posts(req, res) {
 
     const userId = req.session.userId;
     const userLogin = req.session.userLogin;
@@ -12,38 +12,67 @@ function posts(req, res) {
     const perPage = +config.PER_PAGE;
     const page = req.params.page || 1;
 
-    models.Post.find({})
-        .skip(perPage * page - perPage)
-        .limit(perPage)
-        .populate('owner')
-        .sort({ createdAt: -1})
-        .then(posts => {
-            models.Post.count()
-                .then(count => {
-                    res.render('archive/index', {
-                        posts,
-                        current: page,
-                        pages: Math.ceil(count / perPage),
-                        user: {
-                            id: userId,
-                            login: userLogin
-                        }
-                    });
-                })
-                .catch(() => {
-                    throw new Error('Server error')
-                });
-        })
-        .catch(() => {
-            throw new Error('Server error')
+    try {
+        const posts = await models.Post.find({})
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .populate('owner')
+            .sort({
+                createdAt: -1
+            });
+
+        const count = await models.Post.count();
+
+        res.render('archive/index', {
+            posts,
+            current: page,
+            pages: Math.ceil(count / perPage),
+            user: {
+                id: userId,
+                login: userLogin
+            }
         });
+    } catch (error) {
+        throw new Error('Server error ' + error.message);
+    }
+
+
+
+
+    // models.Post.find({})
+    //     .skip(perPage * page - perPage)
+    //     .limit(perPage)
+    //     .populate('owner')
+    //     .sort({
+    //         createdAt: -1
+    //     })
+    //     .then(posts => {
+    //         models.Post.count()
+    //             .then(count => {
+    //                 res.render('archive/index', {
+    //                     posts,
+    //                     current: page,
+    //                     pages: Math.ceil(count / perPage),
+    //                     user: {
+    //                         id: userId,
+    //                         login: userLogin
+    //                     }
+    //                 });
+    //             })
+    //             .catch(() => {
+    //                 throw new Error('Server error')
+    //             });
+    //     })
+    //     .catch(() => {
+    //         throw new Error('Server error')
+    //     });
 }
 
 router.get('/', (req, res) => posts(req, res));
 router.get('/archive/:page', (req, res) => posts(req, res));
 
 // One post page
-router.get('/posts/:post', (req, res, next) => {
+router.get('/posts/:post', async (req, res, next) => {
     const url = req.params.post.trim().replace(/ +(?= )/g, '');
     const userId = req.session.userId;
     const userLogin = req.session.userLogin;
@@ -54,11 +83,12 @@ router.get('/posts/:post', (req, res, next) => {
         err.status = 404;
         next(err);
     } else {
-        models.Post.findOne({
-            url
-        })
-        .populate('owner')
-        .then(post => {
+
+        try {
+            const post = await models.Post.findOne({
+                url
+            }).populate('owner');
+
             if (!post) {
                 const err = new Error('Not found');
                 err.status = 404;
@@ -72,14 +102,37 @@ router.get('/posts/:post', (req, res, next) => {
                     }
                 })
             }
-        }).catch(console.log)
+
+        } catch (error) {
+            throw new Error("Server Error " + error.message);
+        }
+
+        // models.Post.findOne({
+        //         url
+        //     })
+        //     .populate('owner')
+        //     .then(post => {
+        //         if (!post) {
+        //             const err = new Error('Not found');
+        //             err.status = 404;
+        //             next(err);
+        //         } else {
+        //             res.render('post/post', {
+        //                 post,
+        //                 user: {
+        //                     id: userId,
+        //                     login: userLogin
+        //                 }
+        //             })
+        //         }
+        //     }).catch(console.log)
     }
 });
 
 
 // post of users
 
-router.get('/users/:login/:page*?', (req, res) => {
+router.get('/users/:login/:page*?', async (req, res) => {
 
     const userId = req.session.userId;
     const userLogin = req.session.userLogin;
@@ -88,41 +141,77 @@ router.get('/users/:login/:page*?', (req, res) => {
     const page = req.params.page || 1;
     const login = req.params.login;
 
-    models.User.findOne({
-        login
-    }).then(user => {
-        models.Post.find({
+    try {
+        const user = await models.User.findOne({
+            login
+        });
+
+        const posts = await models.Post.find({
                 owner: user.id
             })
             .skip(perPage * page - perPage)
             .limit(perPage)
             .populate('owner')
-            .sort({createdAt: -1})
-            .then(posts => {
-                models.Post.count({
-                        owner: user.id
-                    })
-                    .then(count => {
-                        res.render('archive/user', {
-                            posts,
-                            _user: user,
-                            current: page,
-                            pages: Math.ceil(count / perPage),
-                            user: {
-                                id: userId,
-                                login: userLogin
-                            }
-                        });
-                    })
-                    .catch(() => {
-                        throw new Error('Server error')
-                    });
-            })
-            .catch(() => {
-                throw new Error('Server error')
+            .sort({
+                createdAt: -1
             });
 
-    })
+        const count = models.Post.count({
+            owner: user.id
+        })
+
+        res.render('archive/user', {
+            posts,
+            _user: user,
+            current: page,
+            pages: Math.ceil(count / perPage),
+            user: {
+                id: userId,
+                login: userLogin
+            }
+        });
+
+    } catch (error) {
+        throw new Error('Server Error')
+    }
+
+    // models.User.findOne({
+    //     login
+    // }).then(user => {
+    //     models.Post.find({
+    //             owner: user.id
+    //         })
+    //         .skip(perPage * page - perPage)
+    //         .limit(perPage)
+    //         .populate('owner')
+    //         .sort({
+    //             createdAt: -1
+    //         })
+    //         .then(posts => {
+    //             models.Post.count({
+    //                     owner: user.id
+    //                 })
+    //                 .then(count => {
+    //                     res.render('archive/user', {
+    //                         posts,
+    //                         _user: user,
+    //                         current: page,
+    //                         pages: Math.ceil(count / perPage),
+    //                         user: {
+    //                             id: userId,
+    //                             login: userLogin
+    //                         }
+    //                     });
+    //                 })
+    //                 .catch(() => {
+    //                     throw new Error('Server error')
+    //                 });
+    //         })
+    //         .catch(() => {
+    //             throw new Error('Server error')
+    //         });
+
+    // })
 
 });
 
