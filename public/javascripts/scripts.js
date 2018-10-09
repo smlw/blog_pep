a9e6be816aad1f8e40a84b8109caea4ce6d32f25
Alexander Samoylov <smlwmy@gmail.com>
2018-10-09 16:16:46 +0500

    view images

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -94,75 +94,79 @@ $(function() {
 
 /* eslint-enable no-undef */
 /* eslint-disable no-undef */
-$(function(){
-    
-    function removeErrors(){
+$(function () {
+
+    function removeErrors() {
         $('.post-form p.error').remove();
         $('.post-form input, #post-body').removeClass('error');
     }
 
-  // clear
-  $('.post-form input, #post-body').on('focus', function() {
-    removeErrors()
-});
+    // clear
+    $('.post-form input, #post-body').on('focus', function () {
+        removeErrors()
+    });
 
-  // publish
-  $('.publish-button, .save-button').on('click', function(e) {
-    e.preventDefault();
-    removeErrors()
+    // publish
+    $('.publish-button, .save-button').on('click', function (e) {
+        e.preventDefault();
+        removeErrors()
 
-    var isDraft = $(this).attr('class').split(' ')[0] === 'save-button'
+        var isDraft = $(this).attr('class').split(' ')[0] === 'save-button'
 
-    var data = {
-      title: $('#post-title').val(),
-      body: $('#post-body').val(),
-      isDraft: isDraft,
-      postId: $('#post-id').val()
-    };
+        var data = {
+            title: $('#post-title').val(),
+            body: $('#post-body').val(),
+            isDraft: isDraft,
+            postId: $('#post-id').val()
+        };
 
-    $.ajax({
-      type: 'POST',
-      data: JSON.stringify(data),
-      contentType: 'application/json',
-      url: '/post/add'
-    }).done(function(data) {
-        console.log(data);
-        if (!data.ok) {
-            $('.post-form h2').after('<p class="error">' + data.error + '</p>');
-            if (data.fields) {
-            data.fields.forEach(function(item) {
-                $('#post-' + item).addClass('error');
-            });
-            }
-        } else {
-            // $('.post-form h2').after('<p class="success">Отлично!</p>');
-            if(isDraft){
-                $(location).attr('href', '/post/edit/' + data.post.id);
+        $.ajax({
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            url: '/post/add'
+        }).done(function (data) {
+            console.log(data);
+            if (!data.ok) {
+                $('.post-form h2').after('<p class="error">' + data.error + '</p>');
+                if (data.fields) {
+                    data.fields.forEach(function (item) {
+                        $('#post-' + item).addClass('error');
+                    });
+                }
             } else {
-                $(location).attr('href', '/posts/' + data.post.url);
+                // $('.post-form h2').after('<p class="success">Отлично!</p>');
+                if (isDraft) {
+                    $(location).attr('href', '/post/edit/' + data.post.id);
+                } else {
+                    $(location).attr('href', '/posts/' + data.post.url);
+                }
             }
-        }
         });
     });
 
     //upload
-    $('#file').on('change', function(e) {
+    $('#file').on('change', function (e) {
         // e.preventDefault(); 
 
         var formData = new FormData();
         formData.append('postId', $('#post-id').val());
         formData.append('file', $('#file')[0].files[0]);
-        
+
         $.ajax({
             type: 'POST',
             url: '/upload/image',
             data: formData,
             processData: false,
             contentType: false,
-            success: function(r){
-                console.log(r)
+            success: function (data) {
+                console.log(data);
+                $('#fileinfo').prepend(
+                    '<div class="img-container"><img src="/uploads' +
+                    data.filePath +
+                    '" alt="" /></div>');
             },
-            error: function(e){
+            error: function (e) {
                 console.log(e)
             }
         })
